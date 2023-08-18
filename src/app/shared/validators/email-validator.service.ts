d30c11807eed7fc25b753fd1ab9d2d5239e43eb7
Andreea <andelenasamur@gmail.com>
2023-08-18 11:34:43 +0200

    check that two fields are equal

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -8,14 +8,40 @@ export class EmailValidator implements AsyncValidator{
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
 
     const email = control.value;
-    console.log({email})
 
-    return of({
-      emailTaken: true
+    const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) =>{
+
+      console.log({email});
+
+      if ( email === 'andreea@google.com') {
+        subscriber.next({emailTaken: true});
+        subscriber.complete();
+        // return;
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
     }).pipe(
-      delay(2000)
-    );
+      delay(3000)
+    )
+
+    return httpCallObservable;
+
   }
+
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+  //   const email = control.value;
+  //   console.log({email})
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   );
+  // }
 
 
 }
